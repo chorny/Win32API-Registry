acfaf89e9bff6e44c0ec39394ff7cd7e85d0e5ef
Ben Marsh <blmarsh@gmail.com>
2009-09-15 04:47:39 +0000

    Import of BLM/Win32API-Registry-0.30_01 from CPAN.
    gitpan-cpan-distribution: Win32API-Registry
    gitpan-cpan-version:      0.30_01
    gitpan-cpan-path:         BLM/Win32API-Registry-0.30_01.tar.gz
    gitpan-cpan-author:       BLM
    gitpan-cpan-maturity:     developer

--- lib/Win32API/Registry.pm
+++ lib/Win32API/Registry.pm
@@ -4,7 +4,7 @@ package Win32API::Registry;
 
 use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS); #@EXPORT_FAIL);
-$VERSION= '0.30';
+$VERSION= '0.30_01';
 
 require Exporter;
 require DynaLoader;
